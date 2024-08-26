eb49b9de77d10d906d1d7a610a42bb60d68c8b8a
Matsi <maxi.ramos09@gmail.com>
2024-08-26 16:36:13 -0300

    Calculadora terminada

--- Javascript/main.js
+++ Javascript/main.js
@@ -3,8 +3,8 @@ function convertir() {
     const pulgadas = document.getElementById('pulgadas').value;
 
     // esto verifica si el valor ingresado es un número para el resultado
-    if (isNaN(pulgadas) || pulgadas === '') {
-        document.getElementById('resultado').textContent = 'por favor, ingresar un valor válido en números.';
+    if (isNaN(pulgadas) || pulgadas < 0) {
+        document.getElementById('resultado').textContent = 'por favor, ingresar un valor válido en números positivos.';
         return;
     }
 
