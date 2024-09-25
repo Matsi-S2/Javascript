0ae39dc9b5bcf0b55a35972ca8e5a4725c070665
Matsi <maxi.ramos09@gmail.com>
2024-09-25 15:19:26 -0300

    Probando

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,65 +1,161 @@
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-const listaElementos = [];
-
-function agregarElemento() {
-    // Captura el valor del input
-    const inputElemento = document.getElementById("inputElemento");
-    const nuevoElemento = inputElemento.value.trim();
-    
-    // Verifica si se introdujo algún valor
-    if (nuevoElemento !== "") {
-        // Agrega el nuevo elemento al array
-        listaElementos.push(nuevoElemento);
-        
-        
-        inputElemento.value = "";
-        
-        // Actualiza la visualización de la lista en la página
-        actualizarLista();
-    } else {
-        alert("No se introdujo un valor válido.");
+const productos = [
+    {
+        id: "celular-01",
+        titulo: "Samsung Galaxy A04 64gb 4gb ram",
+        precio: 254.999,
+        img:"./img/celular1.webp",
+    },
+    {
+        id: "celular-02",
+        titulo: "Samsung Galaxy A14(Mediatek) 5G 128gb 4gb ram",
+        precio: 387.999,
+        img: "./img/celular2.webp",
+    },
+    {
+        id: "celular-03",
+        titulo: "Samsung Galaxy A35 5G 128gb 6gb ram",
+        precio: 379.999,
+        img: "./img/celular3.webp",
+    },
+    {
+        id: "celular-04",
+        titulo: "Apple Iphone 15 128gb 6gb ram",
+        precio: 991.999,
+        img: "./img/celular4.webp",
+    },
+    {
+        id: "celular-05",
+        titulo: "Apple Iphone 13 128gb 4gb ram",
+        precio: 859.999,
+        img: "./img/celular5.jpg",
+    },
+    {
+        id: "celular-06",
+        titulo: "Apple Iphone 11 128gb 4gb ram",
+        precio: 759.999,
+        img: "./img/celular6.jpg",
     }
-}
+];
 
-// Función para eliminar un elemento de la lista
-function eliminarElemento() {
-    
-    const elementoAEliminar = prompt("Introduce el elemento que deseas eliminar:");
-    
-    
-    if (elementoAEliminar !== null) {
-        const elementoAEliminarTrimmed = elementoAEliminar.trim();
-        
-        // Busca el índice del elemento en la lista
-        const indice = listaElementos.indexOf(elementoAEliminarTrimmed);
-        
-        // Verifica si el elemento se encuentra en la lista
-        if (indice !== -1) {
-            // Elimina el elemento del array
-            listaElementos.splice(indice, 1);
-            
-            // Actualiza la visualización de la lista en la página
-            actualizarLista();
-        } else {
-            alert("El elemento no se encontró en la lista.");
-        }
+const contenedorProductos = document.querySelector("#productos");
+const carritoVacio = document.querySelector("#carrito-vacio");
+const carritoProductos = document.querySelector("#carrito-productos");
+const carritoTotal = document.querySelector("#carrito-total");
+
+productos.forEach((producto) => {
+    let div = document.createElement("div");
+    div.classList.add("producto");
+    div.innerHTML = `
+        <img class="producto-img" src=${producto.img}>
+        <h3>${producto.titulo}</h3>
+        <p>$${producto.precio}</p>
+    `;
+
+    let button = document.createElement("button");
+    button.classList.add("producto-btn");
+    button.innerText = "Agregar al carrito";
+    button.addEventListener("click", () => {
+        agregarAlCarrito(producto);
+    })
+
+    div.append(button);
+
+    contenedorProductos.append(div);
+});
+
+function actualizarCarrito() {
+    if (carrito.length === 0) {
+        carritoVacio.classList.remove("d-none");
+        carritoProductos.classList.add("d-none");
     } else {
-        alert("No se introdujo un valor válido.");
+        carritoVacio.classList.add("d-none");
+        carritoProductos.classList.remove("d-none");
+
+        carritoProductos.innerHTML = "";
+        carrito.forEach((producto) => {
+            let div = document.createElement("div");
+            div.classList.add("carrito-producto");
+            div.innerHTML = `
+                <h3>${producto.titulo}</h3>
+                <p>$${producto.precio}</p>
+                <p>Cant: ${producto.cantidad}</p>
+                <p>Subt: $${producto.precio * producto.cantidad}</p>
+            `;
+
+            let buttonAumentar = document.createElement("button");
+            buttonAumentar.classList.add("carrito-producto-btn");
+            buttonAumentar.innerText = "⬆️";
+            buttonAumentar.addEventListener("click", () => {
+                aumentarCantidad(producto);
+            })
+            div.append(buttonAumentar);
+
+            let buttonReducir = document.createElement("button");
+            buttonReducir.classList.add("carrito-producto-btn");
+            buttonReducir.innerText = "⬇️";
+            buttonReducir.addEventListener("click", () => {
+                reducirCantidad(producto);
+            })
+            div.append(buttonReducir);
+
+            let button = document.createElement("button");
+            button.classList.add("carrito-producto-btn");
+            button.innerText = "✖️";
+            button.addEventListener("click", () => {
+                borrarDelCarrito(producto);
+            });
+
+            div.append(button);
+            carritoProductos.append(div);
+        });
     }
+    actualizarTotal();
+    localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
-a
-function actualizarLista() {
-    
-    const listaUl = document.getElementById("lista");
-    
-    
-    listaUl.innerHTML = "";
-    
-    
-    listaElementos.forEach(elemento => {
-        const li = document.createElement("li");
-        li.textContent = elemento;
-        listaUl.appendChild(li);
-    });
+function agregarAlCarrito(producto) {
+    let itemEncontrado = carrito.find((item) => item.id === producto.id);
+
+    if (itemEncontrado) {
+        itemEncontrado.cantidad++;
+    } else {
+        carrito.push({...producto, cantidad: 1});
+    }
+
+    actualizarCarrito();
 }
+
+function borrarDelCarrito(producto) {
+    let indice = carrito.findIndex((item) => item.id === producto.id);
+    carrito.splice(indice, 1);
+
+    actualizarCarrito();
+}
+
+function actualizarTotal() {
+    let total = carrito.reduce((acc, prod) => acc + (prod.precio * prod.cantidad), 0);
+    carritoTotal.innerText = `$${total}`;
+}
+
+function aumentarCantidad(producto) {
+    let itemEncontrado = carrito.find((item) => item.id === producto.id);
+    itemEncontrado.cantidad++;
+
+    actualizarCarrito();
+}
+
+function reducirCantidad(producto) {
+    let itemEncontrado = carrito.find((item) => item.id === producto.id);
+
+    if (itemEncontrado.cantidad >= 2) {
+        itemEncontrado.cantidad--;
+        actualizarCarrito();
+    } else {
+        borrarDelCarrito(itemEncontrado);
+    }
+
+}
+
+actualizarCarrito();
